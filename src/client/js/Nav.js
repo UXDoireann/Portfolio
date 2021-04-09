bfff14c213561059e76136de8ecb352e1840b348
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-04-09 21:36:19 +0100

    Style: Animate burger menu when clicked

--- src/client/js/Nav.js
+++ src/client/js/Nav.js
@@ -1,14 +1,19 @@
 //Display nav
 
 const openNav = ()=>{
-    const menu = document.getElementById('menu');
-    menu.style.width = "250px";
+    const burger = document.getElementById('burger');
+    burger.classList.toggle("change");
+    burger.classList.remove("burger_menu");
+   const menu = document.getElementById('menu');
+    menu.style.width = "300px";
     menu.style.display = "block";
     menu.style.color = "white";
     menu.style.textDecoration ="none";
     menu.style.fontSize = "25px";
     menu.style.padding = "8px 8px 8px 32px";
-    menu.style.transition = "0.3s";
+    menu.style.transition = "0.4s";
+    menu.style.transitionTimingFunction = "ease-in-out";
+   
 }
 
 export{openNav}
