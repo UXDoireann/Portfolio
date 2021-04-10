778a9dbad07b80c26398d5caf274c2971a88fad0
Doireann-Ni-Ghrioghair <doireanndoireann@gmail.com>
2021-04-10 15:40:34 +0100

    Feat: Complete nav bar on all pages

--- src/client/js/Nav.js
+++ src/client/js/Nav.js
@@ -5,14 +5,15 @@ const openNav = ()=>{
     burger.classList.toggle("change");
     burger.classList.remove("burger_menu");
    const menu = document.getElementById('menu');
-    menu.style.width = "300px";
+   menu.classList.toggle("menu_out");
+  /*  menu.style.width = "300px";
     menu.style.display = "block";
     menu.style.color = "white";
     menu.style.textDecoration ="none";
     menu.style.fontSize = "25px";
     menu.style.padding = "8px 8px 8px 32px";
     menu.style.transition = "0.4s";
-    menu.style.transitionTimingFunction = "ease-in-out";
+    menu.style.transitionTimingFunction = "ease-in-out";*/
    
 }
 
